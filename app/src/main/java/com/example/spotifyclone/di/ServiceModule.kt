b445ecfa2f583deeff0d5820537d2df1db6eaa64
celal <celal.tas123@gmail.com>
2021-11-21 15:05:54 +0300

    Firebase music service class is created

--- app/src/main/java/com/example/spotifyclone/di/ServiceModule.kt
+++ app/src/main/java/com/example/spotifyclone/di/ServiceModule.kt
@@ -2,6 +2,7 @@ package com.example.spotifyclone.di
 
 
 import android.content.Context
+import com.example.spotifyclone.data.remote.MusicDatabase
 import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.audio.AudioAttributes
@@ -18,6 +19,10 @@ import dagger.hilt.android.scopes.ServiceScoped
 @Module
 @InstallIn(ServiceComponent::class)
 object ServiceModule {
+
+    @ServiceScoped
+    @Provides
+    fun provideMusicDatabase() = MusicDatabase()
 
     @ServiceScoped
     @Provides
